5b6f5b44cc6240f220e09d36f6d75817b81dc02e
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-30 22:04:51 +0600

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routers/Routes/Routes';
 
 function App() {
+  const routers = router;
+
   return (
-    <div>
-      
+    <div className='max-w-screen-xl mx-auto' data-theme="light">
+      <RouterProvider router={routers}>
+
+      </RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import img1 from '../../../assets/images/banner/1.jpg';
+import img2 from '../../../assets/images/banner/2.jpg';
+import img3 from '../../../assets/images/banner/3.jpg';
+import img4 from '../../../assets/images/banner/4.jpg';
+import img5 from '../../../assets/images/banner/5.jpg';
+import img6 from '../../../assets/images/banner/6.jpg';
+
+const Slider = () => {
+    return (
+        <div className="carousel w-full">
+  
+</div>
+    );
+};
+
+export default Slider;
