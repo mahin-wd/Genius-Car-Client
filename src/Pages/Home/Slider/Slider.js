4150c58cb77c9074cc3881a4f7eb7e52d90ad3e4
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-31 07:47:08 +0600

    dynamic banner added

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -5,54 +5,58 @@ import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
 import img5 from '../../../assets/images/banner/5.jpg';
 import img6 from '../../../assets/images/banner/6.jpg';
+import './Slider.css'
+import SliderItem from './SliderItem';
 
 const Slider = () => {
+
+  const sliderData = [
+    {
+      img: img1,
+      id: 1,
+      prev: 6,
+      next: 2
+    },
+    {
+      img: img2,
+      id: 2,
+      prev: 1,
+      next: 3
+    },
+    {
+      img: img3,
+      id: 3,
+      prev: 2,
+      next: 4
+    },
+    {
+      img: img4,
+      id: 4,
+      prev: 3,
+      next: 5
+    },
+    {
+      img: img5,
+      id: 5,
+      prev: 4,
+      next: 6
+    },
+    {
+      img: img6,
+      id: 6,
+      prev: 5,
+      next: 1
+    }
+  ]
     return (
         <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} alt='' className="w-full" />
-    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-5">
-      <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide2" className="btn btn-circle bg-orange-600 border-none">❯</a>
-    </div>
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/3">
-      <h1 className='text-7xl font-bold text-white'>
-        Affordable
-        <br />
-      Price For Car
-      <br />
-      Servicing</h1>
-    </div>
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/2">
-      <p className='text-xl text-white font-normal mt-20 w-1/2'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
-    </div>
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 bottom-1/4">
-    <button className="btn btn-error mr-5">Discover More</button>
-    <button className="btn btn-outline btn-info">Latest Project</button>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={img3} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={img4} alt='' className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
+            {
+              sliderData.map(slide => <SliderItem
+              key={slide.id}
+                slide={slide}
+              ></SliderItem>)
+            }
+        </div>
     );
 };
 
---
+++ src/Pages/Home/Slider/SliderItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SliderItem = ({slide}) => {
+    const {img, id, prev, next} = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+        <div className='slider-img'>
+          <img src={img} alt='' className="w-full" />
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-5">
+          <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
+          <a href={`#slide${next}`} className="btn btn-circle bg-orange-600 border-none">❯</a>
+        </div>
+        <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/3">
+          <h1 className='text-7xl font-bold text-white'>
+            Affordable
+            <br />
+          Price For Car
+          <br />
+          Servicing</h1>
+        </div>
+        <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/2">
+          <p className='text-xl text-white font-normal mt-20 w-1/2'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+        </div>
+        <div className="absolute flex justify-start transform -translate-y-1/2 left-24 bottom-1/4">
+        <button className="btn btn-error mr-5">Discover More</button>
+        <button className="btn btn-outline btn-info">Latest Project</button>
+        </div>
+      </div>
+    );
+};
+
+export default SliderItem;
